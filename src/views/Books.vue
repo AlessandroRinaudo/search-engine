6fdc2ba7ce1b0b7f8b79c1bff0c6c9a013ffc561
Willyan Lin <willyanlin@gmail.com>
2021-09-23 15:47:09 +0200

    feat: 🎸 add list of book page

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main>
+    <div class="max-w-2xl p-4 mx-auto lg:max-w-7xl">
+      <h2 class="text-2xl font-extrabold tracking-tight text-gray-900">{{ $route.params.lang + ' books' }}</h2>
+      <div class="grid grid-cols-1 mt-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <div class="relative p-2 text-center rounded-lg shadow group"
+             v-for="book in this.$store.state.books"
+             v-bind:key="book.title">
+          <div class="w-40 mx-auto my-0 bg-gray-200 rounded-md group-hover:opacity-75">
+            <img :src="`https://www.gutenberg.org/cache/epub/${book.link}/pg${book.link}.cover.medium.jpg`"
+                 alt="Book cover" class="object-cover object-center w-full h-full lg:w-full lg:h-full">
+          </div>
+          <div class="flex justify-center mt-4">
+            <div>
+              <h3 class="text-sm font-bold text-gray-900">
+                <a :href="'https://www.gutenberg.org/ebooks/' + book.link">
+                  <span aria-hidden="true" class="absolute inset-0"></span>
+                  {{ book.Title }}
+                </a>
+              </h3>
+              <p class="text-sm italic text-gray-700 ">{{ book.Author }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
+<script>
+  export default {
+    mounted() {
+      const lang = this.$route.params.lang
+      if (lang === 'all') {
+        this.$store.dispatch('fetchBooks')
+      } else {
+        this.$store.dispatch('fetchBooks', lang)
+      }
+    },
+    data() {
+      return {
+        lang: this.$route.params.lang,
+      }
+    },
+  }
+</script>
--- src/views/Languages.vue
+++ src/views/Languages.vue
@@ -5,7 +5,7 @@
     </h2>
     <div class="flex flex-row flex-wrap mt-5">
       <div class="p-6 my-6 border">
-        <router-link to="language/all">
+        <router-link to="/language/all">
           All
         </router-link>
       </div>
@@ -13,7 +13,7 @@
            class="p-6 my-6 border"
            v-for="lang in $store.state.languages"
            v-bind:key="lang">
-        <router-link :to="'language/' + lang">
+        <router-link :to="'/language/' + lang">
           {{ lang }}
         </router-link>
       </div>
