e27fe1774b2efe490cf13833d0df0737d4b75401
willyan <willyanlin@gmail.com>
2022-03-23 16:52:23 +0100

    feat: 🦄 add no book found

--- src/components/molecules/BookList.vue
+++ src/components/molecules/BookList.vue
@@ -1,14 +1,31 @@
 <template>
-  <div
-    class="grid grid-cols-1 my-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
-  >
-    <div
-      v-for="book in books"
-      :key="book.title"
-      class="relative p-2 text-center rounded-lg shadow-lg group"
-    >
-      <Cover :book="book" />
-    </div>
+  <div>
+    <template v-if="books.length > 0">
+      <div
+        class="grid grid-cols-1 my-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
+      >
+        <div
+          v-for="book in books"
+          :key="book.title"
+          class="relative p-2 text-center rounded-lg shadow-lg group"
+        >
+          <Cover :book="book" />
+        </div>
+      </div>
+    </template>
+    <template v-else>
+      <div class="flex justify-center pt-8">
+        <p class="text-4xl font-extrabold text-indigo-600">Oops...</p>
+        <div class="sm:ml-6">
+          <div class="sm:border-l sm:border-gray-200 sm:pl-6">
+            <h1 class="text-4xl font-extrabold text-gray-900 tracking-tight">
+              Book not found
+            </h1>
+            <p class="mt-1 text-base text-gray-500">Please try another book.</p>
+          </div>
+        </div>
+      </div>
+    </template>
   </div>
 </template>
 <script>
