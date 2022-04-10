dd6d96279f5f95bc702522470d2144f38ee17aa1
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2022-04-10 12:37:50 +0200

    refactor: optimization for score insertion in bwd index

--- src/controllers/indexation.ts
+++ src/controllers/indexation.ts
@@ -2,7 +2,7 @@ import {Request, Response} from "express";
 import {tokenization} from "../utils/tokenize";
 import IFwdIndexModel, {IFwdIndex} from "../models/FwdIndex";
 import {handleErrors, handleSuccess} from "../utils/requests";
-import IBwdIndexModel, {IBwdIndex} from "../models/BwdIndex";
+import IBwdIndexModel, {IBookScore} from "../models/BwdIndex";
 
 
 /**
@@ -58,7 +58,7 @@ const bwd_index = async (req: Request, res: Response) => {
                 }
             }
         }
-        const data = "Backward index : " + index_updated_cpt + " created/updated successfully with " + index_error_cpt + " error"
+        const data = "Backward index : " + index_updated_cpt + " words created/updated successfully with " + index_error_cpt + " error"
         handleSuccess(req, res, data)
     } catch (e) {
         handleErrors(req, res, e.message)
@@ -69,23 +69,21 @@ const bwd_index = async (req: Request, res: Response) => {
 const findOneAndUpdateScore = async (id_book: string, word: string, count: number) => {
     // Find the corresponding word in backward index
     // Compute score
-    let bwd_index: IBwdIndex =
-        await IBwdIndexModel.findOne({word: word})
-    if (!bwd_index) {
-        bwd_index = {
-            word: word,
-            id_books: new Map<string, number>()
-        }
+    const bookScore: IBookScore = {
+        id_book,
+        score: count
     }
-    bwd_index.id_books.set(id_book, count)
-
-    // Update word with new book id and score
-    const inserted_bwd_index =
-        await IBwdIndexModel.findOneAndUpdate(
-            {word: bwd_index.word},
-            bwd_index,
-            {upsert: true}
-        )
+    await IBwdIndexModel.findOneAndUpdate({
+            word: word
+        },
+        {
+            '$push': {'id_books': bookScore}
+        },
+        {
+            upsert: true,
+            runValidators: true
+        }
+    )
 }
 
 export const indexation = {
